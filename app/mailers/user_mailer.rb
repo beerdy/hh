8952751b3d1a1581d53177a4b9fd6bea4c95a708
mr.beerdy <mr.beerdy@gmail.com>
2019-10-01 13:19:35 +0300

    candream - css & modify select

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,14 @@
 class UserMailer < ApplicationMailer
-  default from: 'From site <hmel.house@yandex.com>'
+  default from: 'Хмель Haus <hmel.house@yandex.com>'
 
   def reservation_email(reservation)
     @reservation = reservation
     @url = 'https://hmelhaus.ru'
-    mail(to: 'reservation@hmelhaus.ru,a.kolosova@hmelhaus.ru,ss48.a@yandex.ru,candream@yandex.ru', subject: "RESERVATION - #{@url}" )
+    mail(to: 'reservation@hmelhaus.ru,a.kolosova@hmelhaus.ru,ss48.a@yandex.ru,candream@yandex.ru', subject: "Бронирование столика" )
   end
   def review_email(review)
     @review = review
     @url = 'https://hmelhaus.ru'
-    mail(to: 'reservation@hmelhaus.ru,a.kolosova@hmelhaus.ru,ss48.a@yandex.ru,candream@yandex.ru', subject: "REVIEW - #{@url}" )
+    mail(to: 'reservation@hmelhaus.ru,a.kolosova@hmelhaus.ru,ss48.a@yandex.ru,candream@yandex.ru', subject: "Отзыв на сайте" )
   end
 end
